c377f77b3f6ed1aee22241c6748223f1f23fd2ef
nikishefu <nikitaardashev2004@yandex.ru>
2026-02-19 23:43:24 +0300

    Fix order of execution

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-python manage.py migrate
-python manage.py collectstatic
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 cp -r /app/collected_static/. /static/static/
 python manage.py runserver 0.0.0.0:8000
